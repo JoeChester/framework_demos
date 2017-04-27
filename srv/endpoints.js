13563129d210963549acd1f50fa04c02ab6bc404
Jonas Kleinkauf <jonas.kleinkauf@bbraun.com>
2017-04-27 12:53:43 +0200

    created boilerplate html skeleton

--- srv/endpoints.js
+++ srv/endpoints.js
@@ -2,9 +2,11 @@ let config = require('./config.json');
 let express = require('express');
 let schema = require('./models');
 let bodyParser = require('body-parser');
+let cors = require('cors');
 
 let app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 let Todos = require('./endpoints/todos');
 app.use('/api/todos', Todos);
